d5c1359e00cc453f4e7784f86cf9a1735180a1e0
jritvik007 <164178743+jritvik007@users.noreply.github.com>
2025-03-31 15:37:35 +0530

    first commit

---
+++ credentials.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using std::string;
+
+ string client_id = "yiQGCScD";
+ string client_secret = "TbfqZKEPGqxqL3LsLAMp7Zf1hTLcbCK8NywqlfAsjOg";
+ string base_url = "https://test.deribit.com/api/v2";
+ string accessToken;
